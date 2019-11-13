d9c066725f3d2fa3ca1b7dddb7fdb720ef8f82b4
John-Leon <johnleon0410@gmail.com>
2019-11-13 08:06:07 +0100

    Dateienstruktur angepasst

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import request
+from flask import url_for
 
-app = Flask("Telefonbuch")
+app = Flask("Velotag")
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template('index.html', name="Jonas")
+    return render_template("index.html", name="Startseite")
+
+@app.route("/hinzufuegen")
+def hinzufuegen():
+	return render_template("hinzufuegen.html", name="Velo hinzufügen")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
