045b07ad89971942bbb60df19f7b7b9f5fbb4a8c
John-Leon <johnleon0410@gmail.com>
2019-12-24 18:24:28 +0100

    an main.py Änderungen gemacht, Bildeinbindung funktioniert noch nicht ganz.

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from flask import url_for
 
 from libs import datenbank
 
+#UPLOAD_FOLDER = '/static/velobilder'
+#ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
+
 app = Flask("Velotag")
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 @app.route("/index")
@@ -18,12 +22,28 @@ def index():
 def hinzufuegen():
     if (request.method == 'POST'):
         datenbank.eintrag_speichern_von_formular(request.form)
-        #marke = request.form["marke"]
-        #farbe = request.form["farbe"]
-        #bildname = marke + "-" + farbe + "-"
         return redirect("/veloliste")
 
     return render_template("hinzufuegen.html")
+        #marke = request.form["marke"]
+        #farbe = request.form["farbe"]
+        #bildname = marke + "-" + farbe + "-"
+"""
+def upload_file():
+    if (request.method == 'POST'):
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+"""
 
 @app.route("/veloliste")
 def veloliste():
