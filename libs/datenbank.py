49c62eb1be18a2cb9a376c19b57c36922e7188d5
John-Leon <johnleon0410@gmail.com>
2020-01-10 14:24:35 +0100

    datenbank.py erweitert und neu auskommentiert

--- libs/datenbank.py
+++ libs/datenbank.py
@@ -1,7 +1,10 @@
+# fügt json Datei ein
 import json
 
+# liest die bestehende Datenbank, mit dem Namen datenbank.txt ein
 def datenbank_lesen():
     data = {}
+    # exception handling hilft, falls ein Fehler auftreten würde
     try:
         with open('datenbank.txt', "r") as open_file:
             data = json.load(open_file)
@@ -10,19 +13,23 @@ def datenbank_lesen():
     finally:
         return data
 
+# speichert einen neuen Eintrag von der HTML-Datei in die Datenbank ein
 def eintrag_speichern(marke, farbe, rahmen, preis, zuschlag, verkaufspreis, spende):
     datenbank = datenbank_lesen()
     datenbank[marke] = {"marke": marke, "farbe": farbe, "rahmen": rahmen, "preis": preis, "zuschlag": zuschlag, "verkaufspreis": verkaufspreis, "spende": spende}   
     print(datenbank)
+    # gibt an, dass eine neue Datei geschrieben wird, respektive der neue Datensatz in der Datenbank gespeichert wird
     with open('datenbank.txt', "w", encoding="utf-8") as open_file:
         json.dump(datenbank, open_file)
 
+# mit der Methode "get", werden die eingetragenen Daten an die python-Datei übertragen
 def eintrag_speichern_von_formular(form_request):
     print(form_request)
     marke = form_request.get('marke')
     farbe = form_request.get('farbe')
     rahmen = form_request.get('rahmen')
     preis = int(form_request.get('preis'))
+    # hier wird mithilfe einer if-Schleife, der Preis berechnet, es bestehen variable Preise
     if preis<50:
         zuschlag = 5
         verkaufspreis = preis + 5
@@ -33,10 +40,11 @@ def eintrag_speichern_von_formular(form_request):
         zuschlag = preis*0.1
         verkaufspreis = preis * 1.1
     spende = form_request.get('spende')
+    # hier wird angegeben, dass alle erfassten und berechneten Informationen auch korrekt und vollständig übertragen werden
     eintrag_speichern(marke, farbe, rahmen, preis, zuschlag, verkaufspreis, spende)
 
-
-def person_suchen(form_request):
+# hier wird ermöglicht, dass ein Fahrrad gesucht werden kann
+def eintrag_suchen(form_request):
     datenbank = datenbank_lesen()
     marke = form_request.get('marke')
 
